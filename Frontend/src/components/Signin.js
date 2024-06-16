2f7859701fd025c65d80147c606e09f9cff14265
ShanDWanigasuriya <shandilhara97@gmail.com>
2024-06-16 19:54:12 +0530

    Create Signup & Signin pages

---
+++ Frontend/src/components/Signin.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import api from './api';
+import spaceBackground from '../assets/space_background.jpg';
+import nasaLogo from '../assets/NASA_logo.png';
+
+const SigninPage = () => {
+    const navigate = useNavigate();
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function submit(e) {
+        e.preventDefault();
+        try {
+            const res = await api.post('/signin', { name, password });
+
+            if (res.data.token) {
+                localStorage.setItem('name', name);
+                localStorage.setItem('token', res.data.token);
+                alert('Sign in successful');
+                navigate('/dashboard');
+            } else {
+                alert('Invalid credentials');
+            }
+        } catch (error) {
+            alert('Error occurred');
+            console.error(error);
+        }
+    }
+
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-space-bg" style={{ backgroundImage: `url(${spaceBackground})`, backgroundSize: 'cover' }}>
+            <div className="bg-white shadow-md rounded-lg px-8 py-8 w-full max-w-md">
+                <div className="mb-8 text-center">
+                    <Link to={{ pathname: "/" }} className="flex items-center">
+                        <img src={nasaLogo} alt="NASA Logo" className="w-24 mx-auto mb-4" />
+                    </Link>
+                    <h2 className="text-3xl font-semibold text-gray-800 mb-4">GALACTIC Explorer</h2>
+                    <p className="text-lg text-gray-500 font-semibold mb-6 leading-normal px-8">Explore the universe with NASA APIs</p>
+                </div>
+                <form onSubmit={submit}>
+                    <div className="mb-4">
+                        <label className="block text-sm font-bold text-gray-700 mb-2" htmlFor="name">
+                            Username
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="name"
+                            type="text"
+                            name="name"
+                            placeholder="Enter your username"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-sm font-bold text-gray-700 mb-2" htmlFor="password">
+                            Password
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                            id="password"
+                            type="password"
+                            placeholder="Enter your password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+                        type="submit"
+                    >
+                        Sign In
+                    </button>
+                    <div className="text-center mt-4">
+                        <Link
+                            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                            to="/signup"
+                        >
+                            Don't have an account? Sign Up
+                        </Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default SigninPage;
